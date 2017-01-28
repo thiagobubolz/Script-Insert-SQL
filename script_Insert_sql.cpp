7976bb58257add6bf54451afd8853f8f1653ddb9
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2017-01-28 18:03:16 -0200

    Terminando o scritp em c++ que faz os insert do banco

--- script_Insert_sql.cpp
+++ script_Insert_sql.cpp
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]){
 
 	string auxiliar1, auxiliar2, auxiliar3;
 	int num1, num2, num3;
-	char aux;
+	string aux;
 	int num_treinadores = 0;
 	bool terminou = true;
 	bool continua = true;
@@ -25,10 +25,12 @@ int main(int argc, char const *argv[]){
 		cin >> auxiliar1;
 		cout << "Digite a cidade do treinador\n";
 		cin >> auxiliar2;
-		cout << "Digite a idade do treinador\n\n";
+		cout << "Digite a idade do treinador\n";
 		cin >> num1;
 
-		arquivo << "insert into treinador(nome, idade, cidade) values(" << auxiliar1 << "," << auxiliar2 << "," << num1 << ");\n";
+		cout << "\n\n";
+
+		arquivo << "insert into treinador(nome, idade, cidade) values(" << "'" << auxiliar1 << "'" <<  "," << "'" << auxiliar2 << "'" <<  "," << num1 << ");\n";
 
 		num_treinadores++;
 
@@ -36,21 +38,22 @@ int main(int argc, char const *argv[]){
 			bool pergunta = false;
 			int num_suportado;
 
-			cout<< "Adicionar o " << numero_boxes << " box ? y = sim  n = não \n";
+			cout << "Adicionar o box ? y = sim  n = não\n";
 			cin >> aux;
-			if (aux == 'y'){
+			if (!aux.compare("y")){
 				cout << "Digite o ambiente do box:\n";
 				cin >> auxiliar1;
-				cout << "Digite o numero suportado\n\n";
+				cout << "Digite o numero suportado\n";
 				cin >> num_suportado;
 
-				arquivo << "insert into box(tipo_ambiente, num_suportado, id_prof, id_treinador) values(" << auxiliar1 << "," << num_suportado << "," << 1 << "," << num_treinadores << ");\n";
+				cout << "\n\n";
+				arquivo << "insert into box(tipo_ambiente, num_suportado, id_prof, id_treinador) values(" <<  "'" << auxiliar1 <<  "'" << "," << num_suportado << "," << 1 << "," << num_treinadores << ");\n";
 
 				numero_boxes++;
 
 				cout << "Deseja adicionar uma pokebola ?  y = sim  n = não\n ";
 				cin >> aux;
-				if (aux == 'y'){
+				if (!aux.compare("y")){
 					pergunta = true;
 				}else pergunta = false;
 
@@ -60,13 +63,14 @@ int main(int argc, char const *argv[]){
 
 				 	cout << "Digite o tipo da pokebola:\n";
 					cin >> auxiliar1;
-					cout << "Esta ocupada por um pokemon ?\n\n";
+					cout << "Esta ocupada por um pokemon ?\n";
 					cin >> aux;
-					if (aux == 'y'){
+					if (!aux.compare("y")){
 						ocupada = true;
 					}else ocupada = false;
 
-					arquivo << "insert into pokebola(ocupada, tipo, id_box, id_treinador) values(" << ocupada << "," << auxiliar1 << "," << numero_boxes << "," << num_treinadores << ");\n";
+					cout << "\n\n";
+					arquivo << "insert into pokebola(ocupada, tipo, id_box, id_treinador) values(" << ocupada << "," <<  "'" << auxiliar1 <<  "'" << "," << numero_boxes << "," << num_treinadores << ");\n";
 				 	numero_pokebolas++;
 
 				 	if (ocupada){
@@ -75,9 +79,10 @@ int main(int argc, char const *argv[]){
 						cin >> num1;
 						cout << "Digite o nível do pokemon\n";
 						cin >> num2;
-						cout << "Digite o HP do pokemon\n\n";
+						cout << "Digite o HP do pokemon\n";
 						cin >> num3;
 
+						cout << "\n\n";
 						arquivo << "insert into pokemon(nivel_felicidade, nivel, hp) values(" << num1 << "," << num2 << "," << num3 << ");\n";
 						numero_pokemons++;
 
@@ -87,10 +92,11 @@ int main(int argc, char const *argv[]){
 							cin >> auxiliar1;
 							cout << "Digite a accuracy:\n";
 							cin >> num1;
-							cout << "Digite o dano do ataque:\n\n";
+							cout << "Digite o dano do ataque:\n";
 							cin >> num2;
 
-							arquivo << "insert into ataque(tipo, ccuracy, dano) values(" << auxiliar1 << "," << num1 << "," << num2 << ");\n";
+							cout << "\n\n";
+							arquivo << "insert into ataque(tipo, ccuracy, dano) values(" <<  "'" << auxiliar1 <<  "'" << "," << num1 << "," << num2 << ");\n";
 							numero_ataques_totais++;
 							num_ataques++;
 							arquivo << "insert into tem(id_pokemon, id_ataque) values(" << numero_pokemons << "," << numero_ataques_totais << ");\n";
@@ -101,31 +107,32 @@ int main(int argc, char const *argv[]){
 						cin >> num2;
 						cout << "Digite o nome do pokemon:\n";
 						cin >> auxiliar1;
-						cout << "Digite o CP do pokemon:\n\n";
+						cout << "Digite o CP do pokemon:\n";
 						cin >> num3;
+						cout << "\n\n";
 
 						if(num1 == 1){
-							arquivo << "insert into evolução1(id_pokemon, peso, nome, CP) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << ");\n";
+							arquivo << "insert into evolução1(id_pokemon, peso, nome, CP) values(" << numero_pokemons << "," << num2 << "," <<  "'" << auxiliar1 <<  "'" << "," << num3 << ");\n";
 
 						}else if(num1 == 2){
-							cout << "Digite o tipo 2 do pokemon:\n\n";
+							cout << "Digite o tipo 2 do pokemon:\n";
 							cin >> auxiliar2;
-							arquivo << "insert into evolução2(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << "," << auxiliar2 << ");\n";
+							arquivo << "insert into evolução2(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," <<  "'" << auxiliar1 <<  "'" << "," << num3 << "," <<  "'" << auxiliar2 <<  "'" << ");\n";
 
 						}else if(num1 == 3){
-							cout << "Digite o tipo 2 do pokemon:\n\n";
+							cout << "Digite o tipo 2 do pokemon:\n";
 							cin >> auxiliar2;
-							arquivo << "insert into evolução3(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << "," << auxiliar2 << ");\n";
+							arquivo << "insert into evolução3(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," <<  "'" << auxiliar1 <<  "'" << "," << num3 << "," <<  "'" << auxiliar2 <<  "'" << ");\n";
 						}
 				 	}
-					cout << "Deseja adicionar outra pokebola ?  y = sim  n = não\n\n ";
+					cout << "Deseja adicionar outra pokebola ?  y = sim  n = não\n";
 					cin >> aux;
-					if (aux == 'y'){
+					if (!aux.compare("y")){
 						pergunta = true;
 					}else pergunta = false;
 
 					if(numero_pokebolas > num_suportado){
-					 	cout << "O numero máximo de pokebolas foi alcansado adicione outro box \n\n";
+					 	cout << "O numero máximo de pokebolas foi alcansado adicione outro box \n";
 					 	pergunta = false;
 					}
 				}
@@ -136,9 +143,9 @@ int main(int argc, char const *argv[]){
 
 		};
 
-		cout << "Adicionar outro treinador ?  y = sim  n = não  \n\n";
+		cout << "Adicionar outro treinador ?  y = sim  n = não  \n";
 		cin >> aux;
-		if (aux == 'y'){
+		if (!aux.compare("y")){
 			terminou = true;
 		}else{
 			terminou = false;
