77bac895c22b5cff507cb5329efca0b8ac9ea44f
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2017-01-28 16:45:49 -0200

    programa quase finalizado

---
+++ db_insert.sql
@@ -0,0 +1,29 @@
+insert into treinador(nome, idade, cidade) values(thiago,pelotas,24);
+insert into box(tipo_ambiente, num_suportado, id_prof, id_treinador) values(terra,5,1,1);
+insert into pokebola(ocupada, tipo, id_box, id_treinador) values(1,masterball,2,1);
+insert into pokemon(nivel_felicidade, nivel, hp) values(30,nar o 44);
+insert into ataque(tipo, ccuracy, dano) values(fogo,ambiente do box:
+14);
+insert into tem(id_pokemon, id_ataque) values(1,1);
+insert into ataque(tipo, ccuracy, dano) values(normal, n = não 
+15);
+insert into tem(id_pokemon, id_ataque) values(1,2);
+insert into ataque(tipo, ccuracy, dano) values(agua,
+13);
+insert into tem(id_pokemon, id_ataque) values(1,3);
+insert into ataque(tipo, ccuracy, dano) values(dark,r o 55);
+insert into tem(id_pokemon, id_ataque) values(1,4);
+insert into evolução1(id_pokemon, peso, nome, CP) values(1,45,caterpie,990);
+insert into treinador(nome, idade, cidade) values(thiago,pelotas,24);
+insert into box(tipo_ambiente, num_suportado, id_prof, id_treinador) values(agua,12,1,1);
+insert into pokebola(ocupada, tipo, id_box, id_treinador) values(1,ultraball,2,1);
+insert into pokemon(nivel_felicidade, nivel, hp) values(24,15,55);
+insert into ataque(tipo, ccuracy, dano) values(fogo,33,33);
+insert into tem(id_pokemon, id_ataque) values(1,1);
+insert into ataque(tipo, ccuracy, dano) values(agua,3,44);
+insert into tem(id_pokemon, id_ataque) values(1,2);
+insert into ataque(tipo, ccuracy, dano) values(terra,33,33);
+insert into tem(id_pokemon, id_ataque) values(1,3);
+insert into ataque(tipo, ccuracy, dano) values(normal,33,33);
+insert into tem(id_pokemon, id_ataque) values(1,4);
+insert into evolução1(id_pokemon, peso, nome, CP) values(1,55,caterpie,66);
--- script_Insert_sql.cpp
+++ script_Insert_sql.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <ofstream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -8,50 +7,145 @@ using namespace std;
 int main(int argc, char const *argv[]){
 
 	string auxiliar1, auxiliar2, auxiliar3;
-
+	int num1, num2, num3;
+	char aux;
+	int num_treinadores = 0;
+	bool terminou = true;
 	bool continua = true;
+	int numero_pokebolas = 0;
+	int numero_pokemons = 0;
+	int numero_ataques_totais = 0;
+	int numero_boxes = 1;
 
 	ofstream arquivo;
-	arquivo.open("db_insert.txt");
+	arquivo.open("db_insert.sql", ios::app);
 
-	cout << "Digite o nome do treinador:\n";
-	cin >> auxiliar1;
-	cout << "Digite a cidade do treinador\n";
-	cin >> auxiliar2;
-	cout << "Digite a idade do treinador\n";
-	cin >> auxiliar3;
+	while(terminou){
+		cout << "Digite o nome do treinador:\n";
+		cin >> auxiliar1;
+		cout << "Digite a cidade do treinador\n";
+		cin >> auxiliar2;
+		cout << "Digite a idade do treinador\n\n";
+		cin >> num1;
 
-	arquivo << "insert into treinador(nome, idade, cidade) values(" + auxiliar1 + "," + auxiliar2 + "," + auxiliar3 ");\n";
+		arquivo << "insert into treinador(nome, idade, cidade) values(" << auxiliar1 << "," << auxiliar2 << "," << num1 << ");\n";
 
-	while(continua){
-		int numero_boxes = 1;
-		bool pergunta = false;
-		char aux;
+		num_treinadores++;
 
-		cout<< "Adicionar o " + numero_boxes + "º box ? y = sim  n = não \n";
-		cin >> aux;
-		if (aux == "y"){
-			//Fazer o insert do box
+		while(continua){
+			bool pergunta = false;
+			int num_suportado;
 
-			numero_boxes++;
+			cout<< "Adicionar o " << numero_boxes << " box ? y = sim  n = não \n";
+			cin >> aux;
+			if (aux == 'y'){
+				cout << "Digite o ambiente do box:\n";
+				cin >> auxiliar1;
+				cout << "Digite o numero suportado\n\n";
+				cin >> num_suportado;
 
-			cout << "Deseja adicionar um Pokemon ? \n ";
-			while(pergunta){
-				//Fazer insert dos Pokemon
-				
+				arquivo << "insert into box(tipo_ambiente, num_suportado, id_prof, id_treinador) values(" << auxiliar1 << "," << num_suportado << "," << 1 << "," << num_treinadores << ");\n";
 
+				numero_boxes++;
 
+				cout << "Deseja adicionar uma pokebola ?  y = sim  n = não\n ";
+				cin >> aux;
+				if (aux == 'y'){
+					pergunta = true;
+				}else pergunta = false;
+
+				while(pergunta){
+				 	bool ocupada;
+				 	int num_ataques = 0;
+
+				 	cout << "Digite o tipo da pokebola:\n";
+					cin >> auxiliar1;
+					cout << "Esta ocupada por um pokemon ?\n\n";
+					cin >> aux;
+					if (aux == 'y'){
+						ocupada = true;
+					}else ocupada = false;
+
+					arquivo << "insert into pokebola(ocupada, tipo, id_box, id_treinador) values(" << ocupada << "," << auxiliar1 << "," << numero_boxes << "," << num_treinadores << ");\n";
+				 	numero_pokebolas++;
+
+				 	if (ocupada){
+				 		cout << "Coloque os dados do pokemon:\n";
+				 		cout << "Digite o nivel de nivel de felicidade:\n";
+						cin >> num1;
+						cout << "Digite o nível do pokemon\n";
+						cin >> num2;
+						cout << "Digite o HP do pokemon\n\n";
+						cin >> num3;
+
+						arquivo << "insert into pokemon(nivel_felicidade, nivel, hp) values(" << num1 << "," << num2 << "," << num3 << ");\n";
+						numero_pokemons++;
+
+						while(num_ataques != 4){
+							cout << "Coloque os dados do ataque:\n";
+					 		cout << "Digite o tipo de ataque:\n";
+							cin >> auxiliar1;
+							cout << "Digite a accuracy:\n";
+							cin >> num1;
+							cout << "Digite o dano do ataque:\n\n";
+							cin >> num2;
+
+							arquivo << "insert into ataque(tipo, ccuracy, dano) values(" << auxiliar1 << "," << num1 << "," << num2 << ");\n";
+							numero_ataques_totais++;
+							num_ataques++;
+							arquivo << "insert into tem(id_pokemon, id_ataque) values(" << numero_pokemons << "," << numero_ataques_totais << ");\n";
+						}
+						cout << "Coloque a evolução do pokemon 1 2 ou 3:\n";
+						cin >> num1;
+				 		cout << "Digite o peso do pokemon:\n";
+						cin >> num2;
+						cout << "Digite o nome do pokemon:\n";
+						cin >> auxiliar1;
+						cout << "Digite o CP do pokemon:\n\n";
+						cin >> num3;
+
+						if(num1 == 1){
+							arquivo << "insert into evolução1(id_pokemon, peso, nome, CP) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << ");\n";
+
+						}else if(num1 == 2){
+							cout << "Digite o tipo 2 do pokemon:\n\n";
+							cin >> auxiliar2;
+							arquivo << "insert into evolução2(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << "," << auxiliar2 << ");\n";
+
+						}else if(num1 == 3){
+							cout << "Digite o tipo 2 do pokemon:\n\n";
+							cin >> auxiliar2;
+							arquivo << "insert into evolução3(id_pokemon, peso, nome, CP, tipo2) values(" << numero_pokemons << "," << num2 << "," << auxiliar1 << "," << num3 << "," << auxiliar2 << ");\n";
+						}
+				 	}
+					cout << "Deseja adicionar outra pokebola ?  y = sim  n = não\n\n ";
+					cin >> aux;
+					if (aux == 'y'){
+						pergunta = true;
+					}else pergunta = false;
+
+					if(numero_pokebolas > num_suportado){
+					 	cout << "O numero máximo de pokebolas foi alcansado adicione outro box \n\n";
+					 	pergunta = false;
+					}
+				}
+
+			}else{
+				continua = false;
 			}
 
+		};
+
+		cout << "Adicionar outro treinador ?  y = sim  n = não  \n\n";
+		cin >> aux;
+		if (aux == 'y'){
+			terminou = true;
 		}else{
-			return 0;
+			terminou = false;
 		}
-			
 
-		cout << "Deseja adicionar outro box para este treinador ? \n";
+	}
 
-
-	};
-
+	arquivo.close();
 	return 0;
 }
